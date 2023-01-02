482d6786b414c759e13dd66424d3684cdcf9ba4e
TOBYhhw <1505994122@qq.com>
2023-01-02 21:58:35 +0800

    基本路由规则配置完成

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 export default function Home() {
     return (
-        <div>Home</div>
+        <div>
+            Home
+            <Outlet />
+        </div>
+
     )
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,11 @@
 import { Navigate } from 'react-router-dom'
 import Home from '../pages/Home/'
 import Login from '../pages/Login'
+import Register from '../pages/Register'
+import Index from '../pages/Index'
+import Site from '../pages/Site'
+import User from '../pages/User'
+
 // 路由映射表 
 const routes = [
     {
@@ -8,8 +13,31 @@ const routes = [
         element: <Login />,
     },
     {
+        path: '/register',
+        element: <Register />,
+    },
+    {
         path: '/home',
         element: <Home />,
+        children: [
+            {
+                path: 'index',
+                element: <Index />,
+            },
+            {
+                path: 'site',
+                element: <Site />,
+            },
+            {
+                path: 'user',
+                element: <User />,
+            },
+            {
+                path: '',
+                element: <Navigate to='index' />
+            }
+        ]
+
     },
     // 路由重定向
     {
