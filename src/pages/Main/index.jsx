01c66b00232b3fd819ab5be59b9ab72b36a04e61
Flame-Y <1248993272@qq.com>
2023-01-05 16:51:32 +0800

    feat: 继续完成登录页面

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import "./index.css";
-import logo from '../../assets/turingLogo2.svg'
+import logo from "../../assets/turingLogo2.svg";
+import welcome from "../../assets/welcome.png";
 export default function Main() {
   return (
     <div>
@@ -9,13 +10,12 @@ export default function Main() {
         <div className='sidebar'>
           <div className='app-intro'>
             <div className='app-logo'>
-              <img src={logo} alt="" />
+              <img src={logo} alt='' />
             </div>
-            <div className='app-name'>Turing/のOSS</div>
+            <div className='app-name'>Turing OSS</div>
           </div>
           <div className='welcome'>
-            <div className='hello'>你好</div>
-            <div className='greeting'>欢迎使用 Turing/のOSS</div>
+            <img src={welcome} alt='' />
           </div>
         </div>
         <div className='content'>
