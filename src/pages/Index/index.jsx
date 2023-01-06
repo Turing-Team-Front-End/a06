5d14fbdb5feede78eb7907e8fa79d02ccaab2b54
TOBYhhw <1505994122@qq.com>
2023-01-06 23:36:21 +0800

    优化了css样式

--- src/pages/Index/index.jsx
+++ src/pages/Index/index.jsx
@@ -3,9 +3,9 @@ import { Outlet } from 'react-router-dom'
 import './index.css'
 export default function Index() {
     return (
-        <div className='content1'>
+        <>
             <div className='bucket-side'>bucket-side</div>
             <Outlet />
-        </div>
+        </>
     )
 }
--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import './index.css'
 export default function User() {
     return (
-        <div className='content1'>
+        <>
             <div className='user-outline'>
                 <div className='user-content'>
                     用户管理
                 </div>
             </div>
-        </div>
+        </>
     )
 }
