737cb284cfe66ffe2eb05b1f94f86268e23ff420
TOBYhhw <1505994122@qq.com>
2023-04-06 23:22:17 +0800

    feat:完成获取文件数据列表

--- src/request/api/files.js
+++ src/request/api/files.js
@@ -1,8 +1,8 @@
-import myAxios from "../http";
+import myAxios2 from "../http";
 
 //文件下载
 export function downloadAPI(data) {
-    return myAxios({
+    return myAxios2({
         url: "/download",
         method: 'get',
         data
@@ -10,9 +10,19 @@ export function downloadAPI(data) {
 }
 //删除文件
 export function filesDeleteAPI(data) {
-    return myAxios({
+    return myAxios2({
         url: "/files/delete",
         method: 'delete',
         data
+    })
+}
+//获取bucket下所有文件
+export function filesListallAPI(bid, page, size) {
+    return myAxios2({
+        url: `/files/listall/${bid}/${page}/${size}`,
+        method: 'get',
+        headers: {
+            token: window.sessionStorage.getItem("token")
+        }
     })
 }
