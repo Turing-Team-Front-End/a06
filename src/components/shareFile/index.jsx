92553db67a1792ba4ec5446cdd2947976abdb734
Flame-Y <1248993272@qq.com>
2023-04-09 17:05:12 +0800

    fix: 修复分享链接无法获取的bug

--- src/components/shareFile/index.jsx
+++ src/components/shareFile/index.jsx
@@ -2,73 +2,71 @@ import React, { useEffect } from "react"
 import { Button, Form, Input, message } from "antd"
 import { shareFileAPI } from "../../request/api/files"
 export default function shareFile(props) {
-    let expire
-    const [form] = Form.useForm()
-    const toShare = async (values) => {
-        let data = {
-            bid: props.record.bid,
-            expire: values.expire,
-            fileld: props.record.id
-        }
-        console.log(data);
-        try {
-            let res = await shareFileAPI(data);
-            console.log(res);
-            if (res.data.code === 200) {
-                message.success("获取分享链接成功！")
-
-            } else if (res.data.code === 500) {
-                message.error(res.data.msg)
+  let expire
+  const [form] = Form.useForm()
+  const toShare = async (values) => {
+    let data = {
+      bid: props.record.bid,
+      expire: values.expire,
+      fileId: props.record.id
+    }
+    console.log(data)
+    try {
+      let res = await shareFileAPI(data)
+      console.log(res)
+      if (res.data.code === 200) {
+        message.success("获取分享链接成功！")
+      } else if (res.data.code === 500) {
+        message.error(res.data.msg)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  const onFinishFailed = (errorInfo) => {
+    console.log(errorInfo)
+  }
+  useEffect(() => {
+    console.log(props.record)
+  }, [])
+  return (
+    <div>
+      <div className='login-title'>
+        <p>setExpire</p>
+      </div>
+      <Form
+        style={{ margin: "auto", paddingTop: 40, width: 600 }}
+        form={form}
+        layout='horizontal'
+        className='login-form'
+        initialValues={{
+          remember: true,
+          expire: expire
+        }}
+        onFinish={toShare}
+        onFinishFailed={onFinishFailed}
+      >
+        <p>expire</p>
+        <Form.Item
+          name='expire'
+          rules={[
+            {
+              required: true,
+              message: "expire不能为空"
             }
-        } catch (error) {
-            console.error(error);
-        }
-    }
-    const onFinishFailed = (errorInfo) => {
-        console.log(errorInfo)
-    }
-    useEffect(() => {
-        console.log(props.record);
-    }, [])
-    return (
-        <div>
-            <div className='login-title'>
-                <p>setExpire</p>
-            </div>
-            <Form
-                style={{ margin: "auto", paddingTop: 40, width: 600 }}
-                form={form}
-                layout='horizontal'
-                className='login-form'
-                initialValues={{
-                    remember: true,
-                    expire: expire,
-                }}
-                onFinish={toShare}
-                onFinishFailed={onFinishFailed}
-            >
-                <p>expire</p>
-                <Form.Item
-                    name='expire'
-                    rules={[
-                        {
-                            required: true,
-                            message: "expire不能为空"
-                        }
-                    ]}
-                >
-                    <Input className='login-input' placeholder='请输入expire' />
-                </Form.Item>
-                <Form.Item style={{ textAlign: "center" }}>
-                    <Button className='login-button' htmlType='submit'>
-                        <p>share →</p>
-                    </Button>
-                    {/* <Button shape='round' htmlType='button' onClick={onReset}>
+          ]}
+        >
+          <Input className='login-input' placeholder='请输入expire' />
+        </Form.Item>
+        <Form.Item style={{ textAlign: "center" }}>
+          <Button className='login-button' htmlType='submit'>
+            <p>share →</p>
+          </Button>
+          {/* <Button shape='round' htmlType='button' onClick={onReset}>
             重置
           </Button> */}
-                </Form.Item>
-            </Form>
-        </div>
-    )
+        </Form.Item>
+      </Form>
+    </div>
+  )
 }
-
