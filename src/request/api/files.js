5d156f9cf9e74cc110b4bf5de26f10c8f8aa1e50
Flame-Y <1248993272@qq.com>
2023-04-12 21:36:47 +0800

    fix: 修复下载文件损坏问题

--- src/request/api/files.js
+++ src/request/api/files.js
@@ -1,44 +1,44 @@
-import myAxios2 from "../http";
+import myAxios2 from "../http"
 
 //文件下载
 export function downloadAPI(filename, bid) {
-    return myAxios2({
-        url: `/download/${filename}?bid=${bid}`,
-        method: 'get',
-        headers: {
-            token: window.sessionStorage.getItem("token")
-        }
-    })
+  return myAxios2({
+    url: `/download/${filename}?bid=${bid}`,
+    method: "get",
+    headers: {
+      token: window.sessionStorage.getItem("token")
+    },
+    responseType: "blob"
+  })
 }
 //删除文件
 export function filesDeleteAPI(bid, id) {
-    return myAxios2({
-        url: `/files/delete/${bid}/${id}`,
-        method: 'delete',
-        headers: {
-            token: window.sessionStorage.getItem("token")
-        }
-    })
+  return myAxios2({
+    url: `/files/delete/${bid}/${id}`,
+    method: "delete",
+    headers: {
+      token: window.sessionStorage.getItem("token")
+    }
+  })
 }
 //获取bucket下所有文件
 export function filesListallAPI(bid, page, size) {
-    return myAxios2({
-        url: `/files/listall/${bid}/${page}/${size}`,
-        method: 'get',
-        headers: {
-            token: window.sessionStorage.getItem("token")
-        }
-    })
+  return myAxios2({
+    url: `/files/listall/${bid}/${page}/${size}`,
+    method: "get",
+    headers: {
+      token: window.sessionStorage.getItem("token")
+    }
+  })
 }
 //分享文件链接
 export function shareFileAPI(data) {
-    return myAxios2({
-        url: `/share/generateSharePath`,
-        method: 'get',
-        params: data,
-        headers: {
-            token: window.sessionStorage.getItem("token")
-        }
-
-    })
+  return myAxios2({
+    url: `/share/generateSharePath`,
+    method: "get",
+    params: data,
+    headers: {
+      token: window.sessionStorage.getItem("token")
+    }
+  })
 }
